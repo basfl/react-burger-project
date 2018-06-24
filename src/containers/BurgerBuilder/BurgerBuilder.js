5d1675bccad73e0520ebe3e5f2b37da02748a5ff
babak soltanifar <jupitermaster83@yahoo.com>
2018-06-24 12:31:24 -0400

    fixing purchasable state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,7 @@ class BurgerBuilder extends Component {
     // }
     state = {
        
-        purchasable: false,
+       
         purchasing: false,
         loading: false,
         error: false
@@ -45,7 +45,8 @@ class BurgerBuilder extends Component {
             .reduce( ( sum, el ) => {
                 return sum + el;
             }, 0 );
-        this.setState( { purchasable: sum > 0 } );
+     //   this.setState( { purchasable: sum > 0 } );
+        return sum>0;
     }
 
    
@@ -94,7 +95,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.price} />
                 </Aux>
