971d839cc6c1b5aac18ce28da032be33c18803e5
babak soltanifar <jupitermaster83@yahoo.com>
2018-06-29 14:04:13 -0400

    handling orders from redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -37,3 +37,44 @@ export const purchaseInit=()=>{
     });
 
 }
+export const fetchOrdersSuccess=(orders)=>{
+    return({
+        type:actionTypes.FETCH_ORDERS_SUCCESS,
+        orders:orders
+    });
+}
+export const fetchOrderFail=(error)=>{
+    return({
+        type:actionTypes.FETCH_ORDERS_FAIL,
+        error:error
+    });
+}
+export const fetchOrderStart=()=>{
+    return({
+        type:actionTypes.FETCH_ORDERS_START
+    });
+}
+export const fetchOrders=()=>{
+    return dispatch=>{
+        //////////////////////
+        dispatch(fetchOrderStart());
+         axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders))
+               // this.setState({loading: false, orders: fetchedOrders});
+            })
+            .catch(err => {
+                dispatch(fetchOrderFail(err));
+               // this.setState({loading: false});
+            });
+
+        ///////////////////////
+    }
+}
